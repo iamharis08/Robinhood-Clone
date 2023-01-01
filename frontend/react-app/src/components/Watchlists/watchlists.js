86f3e841148a6bfebb469b4c8bc88632d9ff3679
Haris Ahmed <iamharis08@gmail.com>
2022-12-31 20:59:14 -0500

    show stocks for watchlists complete

--- frontend/react-app/src/components/Watchlists/watchlists.js
+++ frontend/react-app/src/components/Watchlists/watchlists.js
@@ -6,61 +6,93 @@ import "../../css/NavBar.css";
 // import logoIcon from "../../css/images/risinghoodblackicon.png";
 // import HomeNavBar from "./HomeNavBar";
 import "../../css/HomePage.css";
-import "../../css/Watchlists.css"
-import editImg from "../../css/images/edit.svg"
+import "../../css/Watchlists.css";
+import editImg from "../../css/images/edit.svg";
+import arrowImg from "../../css/images/arrow.svg";
+
 import { fetchAllWatchlists } from "../../store/lists";
 
-
 const Watchlists = () => {
-  const dispatch = useDispatch()
-  const watchlists = useSelector(state => state.lists.watchlists)
+  const dispatch = useDispatch();
+  const watchlists = useSelector((state) => state.lists.watchlists);
   const [hoveredList, setHoveredList] = useState(null);
+  const [isClicked, setIsClicked] = useState([]);
 
   useEffect(() => {
-    dispatch(fetchAllWatchlists())
-
+    dispatch(fetchAllWatchlists());
   }, []);
 
-  const watchlistsComponents = Object.values(watchlists)?.map((watchlist, index) => {
-
-    return (
-      <div key={watchlist.id}
-      onMouseEnter={() => setHoveredList(index)}
-      onMouseLeave={() => setHoveredList(null)}
-      >
-        <div className="watchlist-container">
-          <div className="watchlist_header">
-            <div className="watchlist_name">
-              <div className="bulb"><img src={"https://cdn.robinhood.com/emoji/v0/128/1f4a1.png"} alt="bulb" /></div>
-            {watchlist.watchlistName}
+  const watchlistsComponents = Object.values(watchlists)?.map(
+    (watchlist, index) => {
+      return (
+        <div
+          key={watchlist.id}
+          onMouseEnter={() => setHoveredList(index)}
+          onMouseLeave={() => setHoveredList(null)}
+        >
+          <div
+            className="watchlist-header"
+            onClick={() => {
+              if (!isClicked.includes(index)) {
+                setIsClicked([...isClicked, index]);
+              } else {
+                let i = isClicked.indexOf(index);
+                let removedIndex = isClicked;
+                removedIndex.splice(i, 1);
+                setIsClicked([...removedIndex]);
+              }
+            }}
+          >
+            <div className="watchlist-name">
+              <div className="bulb">
+                <img
+                  src={"https://cdn.robinhood.com/emoji/v0/128/1f4a1.png"}
+                  alt="bulb"
+                />
+              </div>
+              {watchlist.watchlistName}
             </div>
-            {hoveredList === index && <div className="watchlist_edit_button"><button><img src={editImg} alt="edit" /></button></div>}
+            <div className="watchlist-buttons">
+              {hoveredList === index && (
+                <div className="watchlist-edit-button">
+                  <button>
+                    <img src={editImg} alt="edit" />
+                  </button>
+                </div>
+              )}
+              <div className="watchlist-arrow">
+                <img src={arrowImg} />
+              </div>
+            </div>
           </div>
+          {isClicked.includes(index) &&
+            watchlist.stocks.map((stock, index) => {
+              return (
+                <div key={index} className="watchlist-header">
+                  <div className="watchlist-name stocks">{stock.stock_symbol} </div>
+
+                </div>
+              );
+            })}
         </div>
-      </div>
-    );
-  });
+      );
+    }
+  );
 
   return (
-
     <div className="lists-container">
       <div className="stocks-list">
         <div className="header-title">
-            <div className="header-text">Stocks</div>
+          <div className="header-text">Stocks</div>
         </div>
-        <div className="user-stocks">
-
-        </div>
+        <div className="user-stocks"></div>
       </div>
       <div className="stocks-list">
         <div className="header-title">
-            <div className="header-text">Lists</div>
+          <div className="header-text">Lists</div>
         </div>
-        <div className="watchlists">
-        {watchlistsComponents}
+        <div className="watchlists">{watchlistsComponents}</div>
       </div>
-      </div>
-
     </div>
   );
 };
