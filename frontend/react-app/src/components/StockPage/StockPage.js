d1e2ebaed05f8fb61964d7bb4d11e614e32acef4
Haris Ahmed <iamharis08@gmail.com>
2023-01-06 17:01:13 -0500

    tansaction styled

--- frontend/react-app/src/components/StockPage/StockPage.js
+++ frontend/react-app/src/components/StockPage/StockPage.js
@@ -13,35 +13,43 @@ const StockPage = () => {
   const stockInfo = useSelector((state) => state.stocks.stockInfo);
   const watchlists = useSelector((state) => state.lists.watchlists);
   const { stockSymbol } = useParams();
-  const [clickedBuyIn, setClickedBuyIn] = useState("Shares");
+  const [clickedBuyIn, setClickedBuyIn] = useState("shares");
   const [clickedDropdown, setClickedDropDown] = useState(false);
   const [showAddModal, setShowAddModal] = useState(false);
-
+  const [isBuy, setIsBuy] = useState(true);
   const [hasStock, setHasStock] = useState([]);
 
-  useEffect(() => {
+  const handleSelect = (e) => {
+    return setClickedBuyIn(e.target.value);
+  };
 
-      let includedStocks = []
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    return;
+  };
+
+  useEffect(() => {
+    let includedStocks = [];
     Object.values(watchlists)?.forEach((watchlist) => {
       if (
         watchlist.stocks.find((stock) => stock.stock_symbol === stockSymbol)
       ) {
-        includedStocks.push(watchlist.id)
+        includedStocks.push(watchlist.id);
       }
     });
-    setHasStock([...includedStocks])
+    setHasStock([...includedStocks]);
   }, [watchlists, stockSymbol]);
 
+  //   useEffect(() => {
 
-
-//   useEffect(() => {
-
-//   }, [hasStock]);
+  //   }, [hasStock]);
 
   useEffect(() => {
     dispatch(clearStockInfo());
     dispatch(fetchStockInfo(stockSymbol));
   }, [dispatch, stockSymbol]);
+
+  useEffect(() => {}, [clickedBuyIn]);
 
   return (
     <div className="stock-page-container">
@@ -141,27 +149,108 @@ const StockPage = () => {
         <div className="second-column">
           <div className="stock-transaction-container">
             <div className="stock-transaction">
-              <div className="transaction-header">Buy {stockSymbol}</div>
+              <div className="transaction-header">
+                <div
+                  className={
+                    isBuy ? "clicked-transaction-type" : "buy-stock-title"
+                  }
+                  onClick={() => setIsBuy(true)}
+                >
+                  Buy {stockSymbol}
+                </div>
+                <div
+                  className={
+                    isBuy ? "buy-stock-title" : "clicked-transaction-type"
+                  }
+                  onClick={() => setIsBuy(false)}
+                >
+                  Sell {stockSymbol}
+                </div>
+              </div>
+
               <div className="transaction-form">
                 <div className="order-type">
                   Order Type <span>Market Order</span>
                 </div>
                 <div className="buy-in">
-                  <span>Buy In </span>
+                  {isBuy ? <span>Buy In </span> : <span>Sell In </span>}
                   <div
                     className="buy-type-dropdown-container"
                     onClick={() => setClickedDropDown(!clickedDropdown)}
-                  >
-                    {clickedBuyIn}
+                  ></div>
+                  <div className="buy-type-dropdown">
+                    <select
+                      name="buyType"
+                      id="buy-type-select"
+                      onChange={handleSelect}
+                    >
+                      <option value="shares" selected>
+                        Shares
+                      </option>
+                      <option value="dollars">Dollars</option>
+                    </select>
                   </div>
-                  {clickedDropdown && (
+                </div>
+
+                {clickedBuyIn === "dollars" ? (
+                  <div className="amount">
+                    <div className="amount-text">Amount</div>
+                    <div className="amount-input">
+                      <input
+                        name="amount"
+                        placeholder="$0.00"
+                        type="number"
+                        min="0"
+                        required
+                      />
+                    </div>
+                  </div>
+                ) : (
+                  <div className="share-amount">
+                    <div className="share-amount-text">Shares</div>
+                    <div className="share-amount-input">
+                      <input placeholder="0" type="number" min="0" required />
+                    </div>
+                  </div>
+                )}
+                <div className="market-price">
+                  <div className="market-price-text">Market Price</div>
+                  <div className="market-price-amount">N/A</div>
+                </div>
+                {isBuy ? (
+                  <div className="transaction-total">
+                    <div className="estimated-text">Estimated Cost</div>
+                    <div className="estimated-price">{"$10000000"}</div>
+                  </div>
+                ) : (
+                  <div className="transaction-total">
+                    <div className="estimated-text">Estimated Credit</div>
+                    <div className="estimated-price">{"$1000"}</div>
+                  </div>
+                )}
+                <div className="transactions-submit-button">
+                  <div className="review-order-button" onClick={handleSubmit}>
+                    Review Order
+                  </div>
+                </div>
+
+                {/* {clickedDropdown && (
                     <div className="buy-type-dropdown">
                       <div className="Shares">Shares</div>
                       <div className="Dollars">Dollars</div>
                     </div>
-                  )}
-                </div>
+                  )} */}
               </div>
+              {isBuy ? (
+                <div className="transactions-power">
+                  <div className="transactions-power-text">{"$200"} buying power avaialable</div>
+
+                </div>
+              ) : (
+                <div className="transactions-power">
+                  <div className="transactions-power-text">{"1"} Shares Avaialable -<span>Sell All</span> </div>
+                </div>
+              )}
             </div>
             <div
               className="add-stock-watchlist"
@@ -175,7 +264,7 @@ const StockPage = () => {
 
       {showAddModal && (
         <Modal onClose={() => setShowAddModal(false)}>
-            {console.log(hasStock, "HASSSSSS STOCKKkk STOCK PAGE")}
+          {console.log(hasStock, "HASSSSSS STOCKKkk STOCK PAGE")}
           <WatchlistStockModal
             hasStock={hasStock}
             setShowAddModal={setShowAddModal}
