51889fb770ed0c63193c9a6ee966f2a89f920a57
Haris Ahmed <iamharis08@gmail.com>
2023-03-07 23:18:51 -0500

    auto play fixed

--- frontend/react-app/src/components/SplashPage/SplashPage.js
+++ frontend/react-app/src/components/SplashPage/SplashPage.js
@@ -10,18 +10,21 @@ const SplashPage = () => {
         <NavBar />
       </div>
         <div className="splash-video-container">
-        <video className="splash-video" src={"https://cdn.robinhood.com/assets/generated_assets/brand/_next/static/images/retirement-hero-hq__67df1aeb147a73f52166e1f391f37f0e.mp4"} width="600" height="300" controlslist="nodownload nofullscreen noremoteplayback" autoplay="true" />
+        <video className="splash-video" src={"https://cdn.robinhood.com/assets/generated_assets/brand/_next/static/images/retirement-hero-hq__67df1aeb147a73f52166e1f391f37f0e.mp4"} width="600" height="300" controlslist="nodownload nofullscreen noremoteplayback" autoplay="true" muted />
         </div>
         <div className="repo-container">
           <div className="repo-text">
             Check out more
           </div>
-          <div className="repo-text">
+          <div className="repo-text bottom-text">
             information about Risinghood
           </div>
           <a className="repo-link" href="https://github.com/iamharis08/Robinhood-Clone">
           <button className="repo-button">Learn More</button>
           </a>
+        </div>
+        <div className="splash-footer">
+          <span>Risinghood</span>
         </div>
     </div>
   );
